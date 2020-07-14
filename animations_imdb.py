642f868cfb7e976e883ed967585c9b13d5b2c40f
Cicero Henrique <cicero.henrique@inatel.br>
2020-07-14 08:39:59 -0300

    Update scrap

--- animations_imdb.py
+++ animations_imdb.py
@@ -4,28 +4,18 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-arq = open('cartoon_list.txt', 'w')
-URL = ("https://www.imdb.com/search/title?title_type=tv_series&genres=animation&explore=title_type,genres")
-
-names = []
-
-response = get(URL)
-
-soup = BeautifulSoup(response.text, 'html.parser')
-soup2 = soup.find_all("div", {"class": "lister-item-content"})
-t = 1
-for i in range(0, 40):
-	for x in range(0, len(soup2)):
-		arq.write("%d- %s \n" % (t, soup2[x].a.get_text().encode(encoding='UTF-8',errors='strict')))
-		names.append("%s" % (soup2[x].a.get_text().encode(encoding='UTF-8',errors='strict')))
-		t = t+1
-	URL = ("https://www.imdb.com/search/title?title_type=tv_series&genres=animation&start={}&explore=title_type,genres&ref_=adv_nxt".format(t))
+with open('cartoon_list.txt', 'w', encoding='utf8') as f:
+	URL = ("https://www.imdb.com/search/title?title_type=tv_series&genres=animation&explore=title_type,genres")
 	response = get(URL)
+
 	soup = BeautifulSoup(response.text, 'html.parser')
 	soup2 = soup.find_all("div", {"class": "lister-item-content"})
-
-arq.close
-
-test_df = pd.DataFrame({'movie': names})
-print(test_df.info())
-test_df
+	t = 1
+	for i in range(0, 40):
+		for x in range(0, len(soup2)):
+			f.write("%d- %s\n" % (t, soup2[x].a.get_text()))
+			t = t+1
+		URL = ("https://www.imdb.com/search/title?title_type=tv_series&genres=animation&start={}&explore=title_type,genres&ref_=adv_nxt".format(t))
+		response = get(URL)
+		soup = BeautifulSoup(response.text, 'html.parser')
+		soup2 = soup.find_all("div", {"class": "lister-item-content"})
--- series_imdb.py
+++ series_imdb.py
@@ -4,28 +4,18 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-arq = open('series_list.txt', 'w')
-URL = ("https://www.imdb.com/search/title?title_type=tv_series&sort=num_votes,desc")
-
-names = []
-
-response = get(URL)
-
-soup = BeautifulSoup(response.text, 'html.parser')
-soup2 = soup.find_all("div", {"class": "lister-item-content"})
-t = 1
-for i in range(0, 40):
-	for x in range(0, len(soup2)):
-		arq.write("%d- %s \n" % (t, soup2[x].a.get_text().encode(encoding='UTF-8',errors='strict')))
-		names.append("%s" % (soup2[x].a.get_text().encode(encoding='UTF-8',errors='strict')))
-		t = t+1
-	URL = ("https://www.imdb.com/search/title?title_type=tv_series&sort=num_votes,desc&start={}&ref_=adv_nxt".format(t))
+with open('series_list.txt', 'w', encoding='utf8') as f:
+	URL = ("https://www.imdb.com/search/title?title_type=tv_series&sort=num_votes,desc")
 	response = get(URL)
+
 	soup = BeautifulSoup(response.text, 'html.parser')
 	soup2 = soup.find_all("div", {"class": "lister-item-content"})
-
-arq.close
-
-test_df = pd.DataFrame({'movie': names})
-print(test_df.info())
-test_df
+	t = 1
+	for i in range(0, 40):
+		for x in range(0, len(soup2)):
+			f.write("%d- %s\n" % (t, soup2[x].a.get_text()))
+			t = t+1
+		URL = ("https://www.imdb.com/search/title?title_type=tv_series&sort=num_votes,desc&start={}&ref_=adv_nxt".format(t))
+		response = get(URL)
+		soup = BeautifulSoup(response.text, 'html.parser')
+		soup2 = soup.find_all("div", {"class": "lister-item-content"})
